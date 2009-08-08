9b20ca93966a1f2ac166227c94a99ceccb986ff1
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2009-08-08 22:34:31 +0000

    Add PanelTabs

---
+++ trunk/FarTabs/PanelTabsMain.pas
@@ -0,0 +1,586 @@
+{$I Defines.inc}
+{$Typedaddress Off}
+
+unit PanelTabsMain;
+
+{******************************************************************************}
+{* (c) 2009 Max Rusov                                                         *}
+{*                                                                            *}
+{* PanelTabs Far plugin                                                       *}
+{******************************************************************************}
+
+interface
+
+  uses
+    Windows,
+    MixTypes,
+    MixUtils,
+    MixStrings,
+    MixClasses,
+
+   {$ifdef bUnicodeFar}
+    PluginW,
+    FarKeysW,
+   {$else}
+    Plugin,
+    FarKeys,
+   {$endif bUnicodeFar}
+    FarCtrl,
+    FarConMan,
+    PanelTabsCtrl,
+    PanelTabsClasses;
+
+
+ {$ifdef bUnicodeFar}
+  function GetMinFarVersionW :Integer; stdcall;
+  procedure SetStartupInfoW(var psi: TPluginStartupInfo); stdcall;
+  procedure GetPluginInfoW(var pi: TPluginInfo); stdcall;
+  procedure ExitFARW; stdcall;
+  function OpenPluginW(OpenFrom: integer; Item: integer): THandle; stdcall;
+  function ProcessSynchroEventW(Event :integer; Param :Pointer) :Integer; stdcall;
+  function ConfigureW(Item: integer) :Integer; stdcall;
+ {$else}
+  procedure SetStartupInfo(var psi: TPluginStartupInfo); stdcall;
+  procedure GetPluginInfo(var pi: TPluginInfo); stdcall;
+  procedure ExitFAR; stdcall;
+  function OpenPlugin(OpenFrom: integer; Item: integer): THandle; stdcall;
+  function Configure(Item: integer) :Integer; stdcall;
+ {$endif bUnicodeFar}
+
+
+{******************************************************************************}
+{******************************} implementation {******************************}
+{******************************************************************************}
+
+  uses
+    MixDebug;
+
+ {-----------------------------------------------------------------------------}
+
+  var
+    GlobalCommand  :Integer;
+
+
+  function IsActiveConsole :boolean;
+  begin
+    Result := WindowIsChildOf(hConsoleWnd, GetForegroundWindow) and ConManIsActiveConsole;
+  end;
+
+
+  function AlertableSleep(APeriod :Cardinal) :Boolean;
+  begin
+    Result := WaitForSingleObject(hStdin, APeriod) <> WAIT_TIMEOUT;
+  end;
+
+
+  procedure SendKeys(AKey, ACtrl :Word);
+  var
+    vRec :array[0..1] of INPUT_RECORD;
+    vRes :DWORD;
+  begin
+    FillChar(vRec, SizeOf(vRec), 0);
+    with vRec[0], Event.KeyEvent do begin
+      EventType := KEY_EVENT;
+      bKeyDown := True;
+      wVirtualKeyCode := AKey;
+      dwControlKeyState := ACtrl;
+    end;
+    with vRec[1], Event.KeyEvent do begin
+      EventType := KEY_EVENT;
+      bKeyDown := False;
+    end;
+    WriteConsoleInput(hStdin, vRec[0], 2, vRes);
+  end;
+
+
+  type
+    TEventType = (
+      cetKeyDown,
+      cetKeyUp,
+      cetPrefixKeyDown,
+      cetMouseMove,
+      cetMousePress,
+      cetOther
+    );
+    TEventTypes = set of TEventType;
+
+
+  function CheckInput :TEventTypes;
+  var
+    I, vCount :Integer;
+    vEventType :TEventType;
+    vBuf :array[0..16] of Windows.TInputRecord;
+  begin
+    Result := [];
+    if PeekConsoleInput(hStdin, vBuf[0], High(vBuf), DWORD(vCount)) then begin
+      for I := 0 to vCount - 1 do begin
+        with vBuf[I] do begin
+          case EventType of
+            KEY_EVENT:
+              with Event.KeyEvent do
+                if bKeyDown then begin
+                  if wVirtualKeyCode in [VK_Shift, VK_Control, VK_Menu] then
+                    vEventType := cetPrefixKeyDown
+                  else
+                    vEventType := cetKeyDown
+                end else
+                  vEventType := cetKeyUp;
+            _MOUSE_EVENT:
+              with Event.MouseEvent do
+                if (MOUSE_MOVED and dwEventFlags = 0) or (dwButtonState <> 0) then
+                  vEventType := cetMousePress
+                else
+                  vEventType := cetMouseMove
+          else
+            vEventType := cetOther;
+          end;
+        end;
+        Result := Result + [vEventType];
+      end;
+    end;
+  end;
+
+
+ {-----------------------------------------------------------------------------}
+ { THistThread                                                                 }
+ {-----------------------------------------------------------------------------}
+
+  type
+    TTabsThread = class(TThread)
+    public
+      procedure Execute; override;
+      function CallPlugin(ACmd :Integer) :Boolean;
+    end;
+
+
+  procedure TTabsThread.Execute; {override;}
+  var
+    vInput :TEventTypes;
+    vTitle, vLastTitle :TString;
+    vWasInput :Boolean;
+    vPoint :TPoint;
+    vIndex :Integer;
+    vKind :TTabKind;
+    X, Y :Integer;
+    vCh :TChar;
+  begin
+    vLastTitle := '';
+    vWasInput := False;
+
+    while not Terminated do begin
+
+      if ((GetKeyState(VK_LBUTTON) < 0) or (GetKeyState(VK_RBUTTON) < 0)) and IsActiveConsole then begin
+//      Trace('Mouse down...');
+        vPoint := GetConsoleMousePos;
+        if TabsManager.HitTest(vPoint.X, vPoint.Y, vKind, vIndex) <> hsNone then
+          CallPlugin(2);
+        while not Terminated and ((GetKeyState(VK_LBUTTON) < 0) or (GetKeyState(VK_RBUTTON) < 0)) do
+          Sleep(10);
+//      Trace('Mouse Up.');
+      end else
+      begin
+
+        vInput := CheckInput;
+        if vInput = [] then begin
+          vTitle := GetConsoleTitleStr;
+          if vTitle <> vLastTitle then begin
+//          Trace('Title changed...');
+            vLastTitle := vTitle;
+            CallPlugin(1);  
+            vWasInput := False;
+          end else
+          begin
+            if TabsManager.NeedCheck(X, Y) then begin
+              vCh := ReadScreenChar(X, Y);
+              if vCh <> '+' then begin
+//              Trace('Screen need repaint...');
+                CallPlugin(1);
+              end;
+              vWasInput := False;
+            end else
+            if vWasInput then begin
+//            Trace('Check...');
+              CallPlugin(1);
+              vWasInput := False;
+            end;
+          end;
+        end else
+          vWasInput := True;
+
+        if vInput <> [] then begin
+          Sleep(10)
+        end else
+        if AlertableSleep(1000) then begin
+//        Trace('Alert!');
+        end;
+      end;
+    end;
+  end;
+
+
+ {$ifdef bUnicodeFar}
+  function TTabsThread.CallPlugin(ACmd :Integer) :Boolean;
+  begin
+    Result := False;
+    if not TabsManager.CanPaintTabs(True) then
+      Exit;
+
+    FARAPI.AdvControl(hModule, ACTL_SYNCHRO, Pointer(ACmd));
+  end;
+
+ {$else}
+
+  function TTabsThread.CallPlugin(ACmd :Integer) :Boolean;
+  var
+    vKey, vCtrl :Word;
+  begin
+    Result := False;
+
+    vKey  := TabKey1;
+    vCtrl := TabShift1;
+    if vKey = 0 then
+      Exit;
+
+    if not TabsManager.CanPaintTabs(True) then
+      Exit;
+
+//  TraceF('Call plugin: ACmd=%d', [ACmd]);
+    GlobalCommand := ACmd;
+    { Вызовем плагин в основном потоке, через механизм макросов... }
+    SendKeys(vKey, vCtrl);
+
+    { Ждем пока посланое сообщение не будет выбрано из буфера }
+    while not Terminated and ([cetKeyDown, cetKeyUp] * CheckInput <> []) do
+      Sleep(1);
+
+    Result := True;
+  end;
+ {$endif bUnicodeFar}
+
+
+ {-----------------------------------------------------------------------------}
+
+  var
+    TabsThread :TTabsThread;
+
+  procedure SetTabsThread(AOn :Boolean);
+  begin
+    if AOn <> (TabsThread <> nil) then begin
+      if AOn then begin
+        TabsThread := TTabsThread.Create(False);
+      end else
+      begin
+        FreeObj(TabsThread);
+      end;
+    end;
+  end;
+
+
+ {-----------------------------------------------------------------------------}
+ {                                                                             }
+ {-----------------------------------------------------------------------------}
+
+  procedure OptionsMenu;
+  const
+    cMenuCount = 3;
+  var
+    vRes, I :Integer;
+    vItems :PFarMenuItemsArray;
+    vItem :PFarMenuItemEx;
+  begin
+    vItems := MemAllocZero(cMenuCount * SizeOf(TFarMenuItemEx));
+    try
+      vItem := @vItems[0];
+      SetMenuItemChrEx(vItem, GetMsg(strMShowTabs));
+      SetMenuItemChrEx(vItem, GetMsg(strMShowNumbers));
+//    SetMenuItemChrEx(vItem, GetMsg(strMShowButton));
+      SetMenuItemChrEx(vItem, GetMsg(strMSeparateTabs));
+
+      vRes := 0;
+      while True do begin
+        vItems[0].Flags := SetFlag(0, MIF_CHECKED1, optShowTabs);
+        vItems[1].Flags := SetFlag(0, MIF_CHECKED1, optShowNumbers);
+//      vItems[2].Flags := SetFlag(0, MIF_CHECKED1, optShowButton);
+        vItems[2].Flags := SetFlag(0, MIF_CHECKED1, optSeparateTabs);
+
+        for I := 0 to cMenuCount - 1 do
+          vItems[I].Flags := SetFlag(vItems[I].Flags, MIF_SELECTED, I = vRes);
+
+        vRes := FARAPI.Menu(hModule, -1, -1, 0,
+          FMENU_WRAPMODE or FMENU_USEEXT,
+          GetMsg(strOptions),
+          '',
+          ''{'Options'},
+          nil, nil,
+          Pointer(vItems),
+          cMenuCount);
+
+        if vRes = -1 then
+          Exit;
+
+        case vRes of
+          0: TabsManager.ToggleOption(optShowTabs);
+          1: TabsManager.ToggleOption(optShowNumbers);
+//        2: TabsManager.ToggleOption(optShowButton);
+          2: TabsManager.ToggleOption(optSeparateTabs);
+        end;
+
+//      Exit;
+      end;
+
+    finally
+      MemFree(vItems);
+    end;
+  end;
+
+
+  procedure SelectTabByKey;
+  var
+    vKey :Integer;
+  begin
+    vKey := FARAPI.AdvControl(hModule, ACTL_WAITKEY, nil);
+    case vKey of
+      KEY_ESC:
+        {};
+      KEY_INS:
+        TabsManager.AddTab(True);
+      KEY_SPACE:
+        TabsManager.ListTab(True);
+    else
+      TabsManager.SelectTab(True, VKeyToIndex(vKey));
+    end;
+  end;
+
+
+  procedure OpenMenu;
+  var
+    vItems :array[0..4] of TFarMenuItemEx;
+    vRes :Integer;
+  begin
+    TabsManager.PaintTabs;
+
+    FillChar(vItems, SizeOf(vItems), 0);
+    SetMenuItemChr(@vItems[0], GetMsg(strMAddTab));
+    SetMenuItemChr(@vItems[1], GetMsg(strMEditTabs));
+    SetMenuItemChr(@vItems[2], GetMsg(strMSelectTab));
+    SetMenuItemChr(@vItems[3], '', MIF_SEPARATOR);
+    SetMenuItemChr(@vItems[4], GetMsg(strMOptions));
+
+    vRes := FARAPI.Menu(hModule, -1, -1, 0,
+      FMENU_WRAPMODE or FMENU_USEEXT,
+      GetMsg(strTitle),
+      '',
+      'Contents',
+      nil, nil,
+      @vItems,
+      High(vItems)+1);
+
+    case vRes of
+      0: TabsManager.AddTab(True);
+      1: TabsManager.ListTab(True);
+      2: SelectTabByKey;
+      4: OptionsMenu;
+    end;
+  end;
+
+
+  procedure OpenCmdLine(const AStr :TString);
+  var
+    vStr :PTChar;
+    vCmd :TString;
+  begin
+    if AStr <> '' then begin
+      vStr := PTChar(AStr);
+      while vStr^ <> #0 do begin
+        vCmd := ExtractParamStr(vStr);
+        if vCmd <> '' then begin
+          if (vCmd[1] = '/') or (vCmd[1] = '-') then begin
+            Delete(vCmd, 1, 1);
+            TabsManager.RunCommand(vCmd);
+          end;
+        end;
+      end;
+    end else
+      OpenMenu;
+  end;
+
+
+ {-----------------------------------------------------------------------------}
+ { Экспортируемые процедуры                                                    }
+ {-----------------------------------------------------------------------------}
+
+ {$ifdef bUnicodeFar}
+  function GetMinFarVersionW :Integer; stdcall;
+  begin
+    Result := $03ED0200;  { Need 2.0.1005 }   { ProcessSynchroEvent }
+  end;
+ {$endif bUnicodeFar}
+
+
+ {$ifdef bUnicodeFar}
+  procedure SetStartupInfoW(var psi: TPluginStartupInfo); stdcall;
+ {$else}
+  procedure SetStartupInfo(var psi: TPluginStartupInfo); stdcall;
+ {$endif bUnicodeFar}
+  begin
+//  TraceF('SetStartupInfo: Module=%d, RootKey=%s', [psi.ModuleNumber, psi.RootKey]);
+
+    hModule := psi.ModuleNumber;
+    Move(psi, FARAPI, SizeOf(FARAPI));
+    Move(psi.fsf^, FARSTD, SizeOf(FARSTD));
+
+    hStdin := GetStdHandle(STD_INPUT_HANDLE);
+    hStdOut := GetStdHandle(STD_OUTPUT_HANDLE);
+    FRegRoot := psi.RootKey;
+
+    { Получаем Handle консоли Far'а }
+    hFarWindow := FARAPI.AdvControl(hModule, ACTL_GETFARHWND, nil);
+(*
+    InitConsoleProc;
+//  hConWindow := GetConsoleWindow;
+    CanCheckWindow := GetConsoleWindow = hFarWindow;
+*)
+    ReadSetup;
+
+    TabsManager := TTabsManager.Create;
+    SetTabsThread(True);
+  end;
+
+
+  var
+    PluginMenuStrings: array[0..0] of PFarChar;
+    ConfigMenuStrings: array[0..0] of PFarChar;
+
+ {$ifdef bUnicodeFar}
+  procedure GetPluginInfoW(var pi: TPluginInfo); stdcall;
+ {$else}
+  procedure GetPluginInfo(var pi: TPluginInfo); stdcall;
+ {$endif bUnicodeFar}
+  begin
+//  TraceF('GetPluginInfo: %s', ['']);
+
+    pi.StructSize:= SizeOf(pi);
+    pi.Flags:= PF_PRELOAD;
+
+    PluginMenuStrings[0]:= GetMsg(strTitle);
+    pi.PluginMenuStrings := @PluginMenuStrings;
+    pi.PluginMenuStringsNumber := 1;
+
+    ConfigMenuStrings[0]:= GetMsg(strTitle);
+    pi.PluginConfigStrings := @ConfigMenuStrings;
+    pi.PluginConfigStringsNumber := 1;
+
+    pi.CommandPrefix := cFarTabPrefix;
+    pi.Reserved := cFarTabGUID;
+
+//  TabsManager.RefreshTabs;
+  end;
+
+
+ {$ifdef bUnicodeFar}
+  procedure ExitFARW; stdcall;
+ {$else}
+  procedure ExitFAR; stdcall;
+ {$endif bUnicodeFar}
+  begin
+    SetTabsThread(False);
+    FreeObj(TabsManager);
+  end;
+
+
+ {$ifdef bUnicodeFar}
+  function OpenPluginW(OpenFrom: integer; Item: integer): THandle; stdcall;
+ {$else}
+  function OpenPlugin(OpenFrom: integer; Item: integer): THandle; stdcall;
+ {$endif bUnicodeFar}
+  begin
+    Result:= INVALID_HANDLE_VALUE;
+    try
+//    TraceF('OpenPlugin: %d, %d', [OpenFrom, Item]);
+
+     {$ifndef bUnicode}
+      SetFileApisToAnsi;
+      try
+     {$endif bUnicode}
+
+      if OpenFrom and OPEN_FROMMACRO <> 0 then begin
+        Item := Item and not OPEN_FROMMACRO;
+        if Item = 0 then begin
+          case GlobalCommand of
+            1: TabsManager.PaintTabs;
+            2: TabsManager.MouseClick;
+          end;
+          GlobalCommand := 0;
+        end else
+        begin
+          case Item of
+            1: TabsManager.AddTab(True);
+            2: TabsManager.ListTab(True);
+            3: SelectTabByKey;
+            4: OptionsMenu;
+          end;
+        end;
+      end else
+      if OpenFrom = OPEN_COMMANDLINE then
+        OpenCmdLine(FarChar2Str(PFarChar(Item)))
+      else
+        OpenMenu;
+
+     {$ifndef bUnicode}
+      finally
+        SetFileApisToOEM;
+      end;
+     {$endif bUnicode}
+
+    except
+      on E :Exception do
+        HandleError(E);
+    end;
+  end;
+
+
+ {$ifdef bUnicodeFar}
+  function ProcessSynchroEventW(Event :integer; Param :Pointer) :Integer; stdcall;
+  begin
+//  TraceF('ProcessSynchroEventW. Event=%d, Param=%d', [Event, Integer(Param)]);
+
+    Result := 0;
+    try
+      if Event <> SE_COMMONSYNCHRO then
+        Exit;
+
+      case TCardinal(Param) of
+        1: TabsManager.PaintTabs(True);
+        2: TabsManager.MouseClick;
+      end;
+
+    except
+      on E :Exception do
+        HandleError(E);
+    end;
+  end;
+ {$endif bUnicodeFar}
+
+
+
+ {$ifdef bUnicodeFar}
+  function ConfigureW(Item: integer) :Integer; stdcall;
+ {$else}
+  function Configure(Item: integer) :Integer; stdcall;
+ {$endif bUnicodeFar}
+  begin
+    Result := 1;
+    try
+      OptionsMenu;
+    except
+      on E :Exception do
+        HandleError(E);
+    end;
+  end;
+
+
+initialization
+finalization
+//FreeObj(TabsThread);
+end.
+
---
+++ trunk/FarTabs/TabListDlg.pas
@@ -0,0 +1,392 @@
+{$I Defines.inc}
+
+unit TabListDlg;
+
+{******************************************************************************}
+{* (c) 2009 Max Rusov                                                         *}
+{*                                                                            *}
+{* PanelTabs Far plugin                                                       *}
+{******************************************************************************}
+
+interface
+
+  uses
+    Windows,
+    MixTypes,
+    MixUtils,
+    MixStrings,
+   {$ifdef bUnicodeFar}
+    PluginW,
+    FarKeysW,
+   {$else}
+    Plugin,
+    FarKeys,
+   {$endif bUnicodeFar}
+    FarColor,
+    FarCtrl,
+    FarMatch,
+    FarDlg,
+    FarGrid,
+    PanelTabsCtrl,
+    PanelTabsClasses,
+    EditTabDlg;
+
+
+  const
+    cDlgMinWidth  = 20;
+    cDlgMinHeight = 5;
+
+    IdFrame = 0;
+    IdList = 1;
+
+  type
+    TTabsList = class(TFarDialog)
+    public
+      constructor Create; override;
+      destructor Destroy; override;
+
+    protected
+      procedure Prepare; override;
+      procedure InitDialog; override;
+      function DialogHandler(Msg :Integer; Param1 :Integer; Param2 :Integer) :Integer; override;
+
+      procedure SelectItem(ACode :Integer); virtual;
+      procedure ReinitListControl; virtual;
+
+      procedure GridCellClick(ASender :TFarGrid; ACol, ARow :Integer; AButton :Integer; ADouble :Boolean); virtual;
+      function GridGetDlgText(ASender :TFarGrid; ACol, ARow :Integer) :TString; virtual;
+      procedure GridPaintCell(ASender :TFarGrid; X, Y, AWidth :Integer; ACol, ARow :Integer; AColor :Integer); virtual;
+
+    protected
+      FGrid           :TFarGrid;
+      FTabs           :TPanelTabs;
+      FHotkeyColor1   :Integer;
+      FHotkeyColor2   :Integer;
+      FMenuMaxWidth   :Integer;
+      FResInd         :Integer;
+      FResCmd         :Integer;
+
+      procedure ResizeDialog;
+      procedure EditCurrent;
+      procedure SetCurrent(AIndex :Integer; AMode :TLocationMode = lmScroll);
+    end;
+
+
+  function ListTabDlg(ATabs :TPanelTabs; var AIndex :Integer) :Boolean;
+
+
+{******************************************************************************}
+{******************************} implementation {******************************}
+{******************************************************************************}
+
+  uses
+    MixDebug;
+
+ {-----------------------------------------------------------------------------}
+ { TTabsList                                                                  }
+ {-----------------------------------------------------------------------------}
+
+  constructor TTabsList.Create; {override;}
+  begin
+    inherited Create;
+    FHotkeyColor1 := FARAPI.AdvControl(hModule, ACTL_GETCOLOR, Pointer(COL_MENUHIGHLIGHT));
+    FHotkeyColor2 := FARAPI.AdvControl(hModule, ACTL_GETCOLOR, Pointer(COL_MENUSELECTEDHIGHLIGHT));
+  end;
+
+
+  destructor TTabsList.Destroy; {override;}
+  begin
+    inherited Destroy;
+  end;
+
+
+  procedure TTabsList.Prepare; {override;}
+  const
+    DX = 20;
+    DY = 10;
+  begin
+    FHelpTopic := 'List';
+    FWidth := DX;
+    FHeight := DY;
+    FItemCount := 2;
+    FDialog := CreateDialog(
+      [
+        NewItemApi(DI_DoubleBox, 2, 1, DX - 4, DY - 2, 0, GetMsg(strTabs)),
+        NewItemApi(DI_USERCONTROL, 3, 2, DX - 6, DY - 4, 0, '' )
+      ]
+    );
+    FGrid := TFarGrid.CreateEx(Self, IdList);
+    FGrid.OnCellClick := GridCellClick;
+    FGrid.OnGetCellText := GridGetDlgText;
+    FGrid.OnPaintCell := GridPaintCell;
+    FGrid.Options := [goRowSelect, goWrapMode, goFollowMouse];
+    FGrid.Columns.FreeAll;
+    FGrid.Columns.Add( TColumnFormat.CreateEx('', '', 0, taLeftJustify, [coOwnerDraw, coColMargin], 1) );
+    FGrid.Columns.Add( TColumnFormat.CreateEx('', '', 0, taLeftJustify, [coColMargin], 2) );
+  end;
+
+
+  procedure TTabsList.InitDialog; {override;}
+  begin
+    SendMsg(DM_SETMOUSEEVENTNOTIFY, 1, 0);
+    ReinitListControl;
+  end;
+
+
+  procedure TTabsList.ResizeDialog;
+  var
+    vWidth, vHeight :Integer;
+    vRect :TSmallRect;
+    vScreenInfo :TConsoleScreenBufferInfo;
+  begin
+    GetConsoleScreenBufferInfo(hStdOut, vScreenInfo);
+
+    vWidth := FMenuMaxWidth + 6;
+    if vWidth > vScreenInfo.dwSize.X - 4 then
+      vWidth := vScreenInfo.dwSize.X - 4;
+    vWidth := IntMax(vWidth, cDlgMinWidth);
+
+    vHeight := FGrid.RowCount + 4;
+    if vHeight > vScreenInfo.dwSize.Y - 2 then
+      vHeight := vScreenInfo.dwSize.Y - 2;
+    vHeight := IntMax(vHeight, cDlgMinHeight);
+
+    SetDlgPos(-1, -1, vWidth, vHeight);
+
+    vRect.Left := 2;
+    vRect.Top := 1;
+    vRect.Right := vWidth - 3;
+    vRect.Bottom := vHeight - 2;
+    SendMsg(DM_SETITEMPOSITION, IdFrame, @vRect);
+    SRectGrow(vRect, -1, -1);
+    if vRect.Bottom - vRect.Top + 2 <= FGrid.RowCount then
+      Inc(vRect.Right);
+    SendMsg(DM_SETITEMPOSITION, IdList, @vRect);
+    FGrid.UpdateSize(vRect.Left, vRect.Top, vRect.Right - vRect.Left + 1, vRect.Bottom - vRect.Top + 1);
+  end;
+
+
+  procedure TTabsList.SetCurrent(AIndex :Integer; AMode :TLocationMode);
+  begin
+    FGrid.GotoLocation(FGrid.CurCol, AIndex, AMode);
+  end;
+
+
+  procedure TTabsList.ReinitListControl; {virtual;}
+  var
+    I, vMaxLen1, vMaxLen2 :Integer;
+    vTab :TPanelTab;
+  begin
+    vMaxLen1 := 0; vMaxLen2 := 0;
+    for I := 0 to FTabs.Count - 1 do begin
+      vTab := FTabs[I];
+      vMaxLen1 := IntMax(vMaxLen1, Length(vTab.Caption));
+      vMaxLen2 := IntMax(vMaxLen2, Length(vTab.Folder));
+    end;
+
+    Inc(vMaxLen1, Length(Int2Str(FTabs.Count)) + 1);
+
+    FGrid.Column[0].Width := vMaxLen1 + 2;
+    FGrid.Column[1].Width := vMaxLen2 + 2;
+
+    FMenuMaxWidth := vMaxLen1 + 2 + vMaxLen2 + 2 + 1;
+
+    FGrid.ResetSize;
+    FGrid.RowCount := FTabs.Count;
+    SetCurrent(FGrid.CurRow);
+
+    ResizeDialog;
+  end;
+
+
+  procedure TTabsList.GridCellClick(ASender :TFarGrid; ACol, ARow :Integer; AButton :Integer; ADouble :Boolean); {virtual;}
+  begin
+    if (AButton = 1) {and ADouble} then
+      SelectItem(1)
+    else
+    if (AButton = 2) then
+      EditCurrent;
+  end;
+
+
+  function TTabsList.GridGetDlgText(ASender :TFarGrid; ACol, ARow :Integer) :TString; {virtual;}
+  var
+    vTab :TPanelTab;
+  begin
+    Result := '';
+    if ARow < FTabs.Count then begin
+      vTab := FTabs[ARow];
+      case ACol of
+        0: {Result := Format('%d %s', [ARow + 1, vTab.Caption])};
+        1: Result := vTab.Folder;
+      end;
+    end;
+  end;
+
+
+  procedure TTabsList.GridPaintCell(ASender :TFarGrid; X, Y, AWidth :Integer; ACol, ARow :Integer; AColor :Integer); {virtual;}
+  var
+    vTab :TPanelTab;
+    vStr :TString;
+  begin
+    if ARow < FTabs.Count then begin
+      vTab := FTabs[ARow];
+      vStr := Format('%s %s', [IndexToChar(ARow), vTab.Caption]);
+      FGrid.DrawChrEx(X, Y, PTChar(vStr), AWidth, 0, 1, AColor, IntIf(FGrid.CurRow = ARow, FHotkeyColor2, FHotkeyColor1));
+    end;
+  end;
+
+
+  procedure TTabsList.SelectItem(ACode :Integer);  {virtual;}
+  begin
+    if FGrid.CurRow < FGrid.RowCount then begin
+      FResInd := FGrid.CurRow;
+      FResCmd := ACode;
+      SendMsg(DM_CLOSE, -1, 0);
+    end else
+      Beep;
+  end;
+
+
+  procedure TTabsList.EditCurrent;
+  begin
+    if FGrid.CurRow < FGrid.RowCount then begin
+      if EditTab(FTabs, FGrid.CurRow) then begin
+        FTabs.StoreReg('');
+        ReinitListControl;
+      end;
+    end else
+      Beep;
+  end;
+
+
+  function TTabsList.DialogHandler(Msg :Integer; Param1 :Integer; Param2 :Integer): Integer; {override;}
+
+    procedure LocInsert;
+    var
+      vIndex :Integer;
+    begin
+      vIndex := FGrid.CurRow;
+      if vIndex < FGrid.RowCount then
+        Inc(vIndex);
+      if NewTab(FTabs, vIndex) then begin
+        FTabs.StoreReg('');
+        ReinitListControl;
+        SetCurrent(vIndex);
+      end;
+    end;
+
+
+    procedure LocDelete;
+    begin
+      if FGrid.CurRow < FGrid.RowCount then begin
+        FTabs.FreeAt( FGrid.CurRow );
+        FTabs.StoreReg('');
+        ReinitListControl;
+      end else
+        Beep;
+    end;
+
+    procedure LocMove(ADelta :Integer);
+    begin
+      if (FGrid.CurRow < FGrid.RowCount) and (FGrid.CurRow + ADelta >= 0) and (FGrid.CurRow + ADelta < FGrid.RowCount) then begin
+        FTabs.Move(FGrid.CurRow, FGrid.CurRow + ADelta);
+        FTabs.StoreReg('');
+        ReinitListControl;
+        SetCurrent(FGrid.CurRow + ADelta);
+      end else
+        Beep;
+    end;
+
+    procedure LocHotkey(AIndex :Integer);
+    begin
+      if AIndex < FGrid.RowCount then begin
+        SetCurrent(AIndex);
+        SelectItem(1);
+      end else
+        Beep;
+    end;
+
+  var
+    vIndex :Integer;
+  begin
+//  TraceF('InfoDialogProc: FHandle=%d, Msg=%d, Param1=%d, Param2=%d', [FHandle, Msg, Param1, Param2]);
+    Result := 1;
+    case Msg of
+      DN_CTLCOLORDIALOG:
+        Result := FARAPI.AdvControl(hModule, ACTL_GETCOLOR, Pointer(COL_MENUTEXT));
+
+      DN_CTLCOLORDLGITEM:
+        if Param1 = IdFrame then
+          Result := CtrlPalette([COL_MENUTITLE, COL_MENUHIGHLIGHT, COL_MENUBOX])
+        else
+          Result := Param2;
+
+      DN_RESIZECONSOLE:
+        begin
+          ResizeDialog;
+          SetCurrent(FGrid.CurRow);
+        end;
+
+      DN_KEY: begin
+//      TraceF('Key = %d', [Param2]);
+        case Param2 of
+          KEY_ENTER:
+            SelectItem(1);
+
+          KEY_F4:
+            EditCurrent;
+          KEY_INS:
+            LocInsert;
+          KEY_DEL:
+            LocDelete;
+
+          KEY_CTRLUP:
+            LocMove(-1);
+          KEY_CTRLDOWN:
+            LocMove(+1);
+
+        else
+          vIndex := VKeyToIndex(Param2);
+          if vIndex <> -1 then
+            LocHotkey(vIndex)
+          else
+            Result := inherited DialogHandler(Msg, Param1, Param2);
+        end;
+      end;
+
+    else
+      Result := inherited DialogHandler(Msg, Param1, Param2);
+    end;
+  end;
+
+
+ {-----------------------------------------------------------------------------}
+ {                                                                             }
+ {-----------------------------------------------------------------------------}
+
+  function ListTabDlg(ATabs :TPanelTabs; var AIndex :Integer) :Boolean;
+  var
+    vDlg :TTabsList;
+  begin
+    Result := False;
+    vDlg := TTabsList.Create;
+    try
+      vDlg.FTabs := ATabs;
+
+      if vDlg.Run = -1 then
+        Exit;
+
+      case vDlg.FResCmd of
+        1: AIndex := vDlg.FResInd;
+      end;
+      Result := True;
+
+    finally
+      FreeObj(vDlg);
+    end;
+  end;
+
+
+end.
+
