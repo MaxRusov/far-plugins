f41448b8d9c4d81e425e9265d2d8ecd79cde6950
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2009-11-02 22:51:38 +0000

    Add GDIPlus

---
+++ PicViewGDI/GDIPlus.dpr
@@ -0,0 +1,41 @@
+{$I Defines.inc}
+{$E pvd}
+
+{$APPTYPE CONSOLE}
+{$ifdef bDelphi}
+ {$ImageBase $41000000}
+{$endif bDelphi}
+
+library GDIPlus;
+
+uses
+  MixErrors,
+ {$ifdef bTrace}
+  MixCheck,
+ {$endif bTrace}
+  MixFormat,
+  GDIPlusMain;
+
+exports
+  pvdInit2,
+  pvdExit2,
+  pvdPluginInfo2,
+  pvdReloadConfig2,
+  pvdGetFormats2,
+
+  pvdFileOpen2,
+  pvdPageInfo2,
+  pvdPageDecode2,
+  pvdPageFree2,
+  pvdFileClose2,
+
+  pvdDisplayInit2,
+  pvdDisplayAttach2,
+  pvdDisplayCreate2,
+  pvdDisplayPaint2,
+  pvdDisplayClose2,
+  pvdDisplayExit2;
+
+{$R GDIPlus.res}
+
+end.
