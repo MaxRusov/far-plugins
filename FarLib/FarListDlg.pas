b119a2a077f3f8579be1d02825d8566a49755abe
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-01-07 10:16:08 +0000

    FarLib update

--- FarLib/FarListDlg.pas
+++ FarLib/FarListDlg.pas
@@ -16,10 +16,13 @@ interface
     MixTypes,
     MixUtils,
     MixStrings,
+    MixClasses,
    {$ifdef bUnicodeFar}
     PluginW,
+    FarKeysW,
    {$else}
     Plugin,
+    FarKeys,
    {$endif bUnicodeFar}
     FarColor,
     FarCtrl,
@@ -52,6 +55,88 @@ interface
       procedure ResizeDialog;
       procedure SetCurrent(AIndex :Integer; AMode :TLocationMode = lmScroll);
     end;
+
+
+  type
+    PFilterRec = ^TFilterRec;
+    TFilterRec = packed record
+      FIdx :Integer;
+      FPos :Word;
+      FLen :Word;
+    end;
+
+    TListFilter = class(TExList)
+    public
+      constructor Create; override;
+
+      procedure Add(AIndex, APos, ALen :Integer);
+
+    private
+      function GetItems(AIndex :Integer) :Integer;
+
+    public
+      property Items[AIndex :Integer] :Integer read GetItems; default;
+    end;
+
+
+    TDrawBuf = class(TBasis)
+    public
+      constructor Create; override;
+      destructor Destroy; override;
+
+      procedure Clear;
+      procedure Add(AChr :TChar; Attr :Byte; ACount :Integer = 1);
+      procedure AddStrExpandTabsEx(AStr :PTChar; ALen :Integer; AColor :Byte);
+      procedure FillAttr(APos, ACount :Integer; AColor :Byte);
+      procedure FillLoAttr(APos, ACount :Integer; ALoColor :Byte);
+
+      procedure Paint(X, Y, ADelta, ALimit :Integer);
+
+    private
+      FTabSize      :Integer;
+      FChars        :PTChar;
+      FAttrs        :PByteArray;
+      FCount        :Integer;
+      FSize         :Integer;
+
+      procedure SetSize(ASize :Integer);
+
+    public
+      property Count :Integer read FCount;
+    end;
+
+
+    TFilteredListDlg = class(TFarListDlg)
+    public
+      constructor Create; override;
+      destructor Destroy; override;
+
+    protected
+      procedure Prepare; override;
+      procedure InitDialog; override;
+      function CloseDialog(ItemID :Integer) :Boolean; override;
+      function DialogHandler(Msg :Integer; Param1 :Integer; Param2 :TIntPtr) :TIntPtr; override;
+
+      procedure SelectItem(ACode :Integer); virtual;
+      procedure UpdateHeader; virtual;
+      procedure ReinitGrid; virtual;
+      procedure ReinitAndSaveCurrent; virtual;
+
+      procedure GridPosChange(ASender :TFarGrid); virtual;
+      procedure GridCellClick(ASender :TFarGrid; ACol, ARow :Integer; AButton :Integer; ADouble :Boolean); virtual;
+      procedure GridGetCellColor(ASender :TFarGrid; ACol, ARow :Integer; var AColor :Integer); virtual;
+      function GridGetDlgText(ASender :TFarGrid; ACol, ARow :Integer) :TString; virtual;
+      procedure GridPaintCell(ASender :TFarGrid; X, Y, AWidth :Integer; ACol, ARow :Integer; AColor :Integer); virtual;
+
+    protected
+      FDrawBuf     :TDrawBuf;
+      FFilter      :TListFilter;
+      FFilterMask  :TString;
+      FTotalCount  :Integer;
+
+      function RowToIdx(ARow :Integer) :Integer;
+    end;
+
 
 
 {******************************************************************************}
@@ -163,6 +248,320 @@ interface
     end;
   end;
 
+
+ {-----------------------------------------------------------------------------}
+ { TListFilter                                                                 }
+ {-----------------------------------------------------------------------------}
+
+  constructor TListFilter.Create; {override;}
+  begin
+    inherited Create;
+    FItemSize  := SizeOf(TFilterRec);
+    FItemLimit := MaxInt div FItemSize;
+  end;
+
+
+  procedure TListFilter.Add(AIndex, APos, ALen :Integer);
+  var
+    vRec :TFilterRec;
+  begin
+    vRec.FIdx := AIndex;
+    vRec.FPos := Word(APos);
+    vRec.FLen := Byte(ALen);
+    AddData(vRec);
+  end;
+
+
+  function TListFilter.GetItems(AIndex :Integer) :Integer;
+  begin
+    Result := PFilterRec(PItems[AIndex]).FIdx;
+  end;
+
+
+ {-----------------------------------------------------------------------------}
+ { TDrawBuf                                                                    }
+ {-----------------------------------------------------------------------------}
+
+  constructor TDrawBuf.Create; {override;}
+  begin
+    inherited Create;
+    FTabSize := DefTabSize;
+  end;
+
+
+  destructor TDrawBuf.Destroy; {override;}
+  begin
+    MemFree(FChars);
+    MemFree(FAttrs);
+    inherited Destroy;
+  end;
+
+
+  procedure TDrawBuf.Clear;
+  begin
+    FCount := 0;
+  end;
+
+
+  procedure TDrawBuf.Add(AChr :TChar; Attr :Byte; ACount :Integer = 1);
+  var
+    I :Integer;
+  begin
+    if FCount + ACount + 1 > FSize then
+      SetSize(FCount + ACount + 1);
+    for I := 0 to ACount - 1 do begin
+      FChars[FCount] := AChr;
+      FAttrs[FCount] := Attr;
+      Inc(FCount);
+    end;
+  end;
+
+
+  procedure TDrawBuf.AddStrExpandTabsEx(AStr :PTChar; ALen :Integer; AColor :Byte);
+  var
+    vEnd :PTChar;
+    vChr :TChar;
+    vAtr :Byte;
+    vDPos, vDstLen, vSize :Integer;
+  begin
+    vDstLen := ChrExpandTabsLen(AStr, ALen, FTabSize);
+    if FCount + vDstLen + 1 > FSize then
+      SetSize(FCount + vDstLen + 1);
+
+    vEnd := AStr + ALen;
+    vDPos := 0;
+    while AStr < vEnd do begin
+      vChr := AStr^;
+      vAtr := AColor;
+
+      if vChr <> charTab then begin
+        Assert(vDPos < vDstLen);
+        Add(vChr, vAtr);
+        Inc(vDPos);
+      end else
+      begin
+        vSize := FTabSize - (vDPos mod FTabSize);
+        Assert(vDPos + vSize <= vDstLen);
+        Add(' ', vAtr, vSize);
+        Inc(vDPos, vSize);
+      end;
+      Inc(AStr);
+    end;
+    FChars[FCount] := #0;
+  end;
+
+
+  procedure TDrawBuf.FillAttr(APos, ACount :Integer; AColor :Byte);
+  begin
+    Assert(APos + ACount <= FCount);
+    FillChar(FAttrs[APos], ACount, AColor);
+  end;
+
+
+  procedure TDrawBuf.FillLoAttr(APos, ACount :Integer; ALoColor :Byte);
+  var
+    I :Integer;
+  begin
+    Assert(APos + ACount <= FCount);
+    for I := APos to APos + ACount - 1 do
+      FAttrs[I] := (FAttrs[I] and $F0) or (ALoColor and $0F);
+  end;
+
+
+  procedure TDrawBuf.Paint(X, Y, ADelta, ALimit :Integer);
+  var
+    I, J, vEnd, vPartLen :Integer;
+    vAtr :Byte;
+    vTmp :TChar;
+  begin
+    vEnd := FCount;
+    if FCount - ADelta > ALimit then
+      vEnd := ADelta + ALimit;
+
+    I := ADelta;
+    while I < vEnd do begin
+      vAtr := FAttrs[I];
+      J := I + 1;
+      while (J < vEnd) and (FAttrs[J] = vAtr) do
+        Inc(J);
+      vPartLen := J - I;
+      if I + vPartLen = FCount then
+        FARAPI.Text(X, Y, vAtr, FChars + I )
+      else begin
+        vTmp := (FChars + I + vPartLen)^;
+        (FChars + I + vPartLen)^ := #0;
+        FARAPI.Text(X, Y, vAtr, FChars + I );
+        (FChars + I + vPartLen)^ := vTmp;
+      end;
+      Inc(X, vPartLen);
+      Inc(I, vPartLen);
+    end;
+  end;
+
+
+  procedure TDrawBuf.SetSize(ASize :Integer);
+  begin
+    ReallocMem(FChars, ASize * SizeOf(TChar));
+    ReallocMem(FAttrs, ASize * SizeOf(Byte));
+    FSize := ASize;
+  end;
+
+
+ {-----------------------------------------------------------------------------}
+ { TFilteredListDlg                                                            }
+ {-----------------------------------------------------------------------------}
+
+  constructor TFilteredListDlg.Create; {override;}
+  begin
+    inherited Create;
+//  FDrawBuf := TDrawBuf.Create;
+//  FFilter := TListFilter.CreateSize(SizeOf(TFilterRec));
+  end;
+
+
+  destructor TFilteredListDlg.Destroy; {override;}
+  begin
+    FreeObj(FFilter);
+    FreeObj(FDrawBuf);
+    inherited Destroy;
+  end;
+
+
+  procedure TFilteredListDlg.Prepare; {override;}
+  begin
+    inherited Prepare;
+    FGrid.OnCellClick := GridCellClick;
+    FGrid.OnPosChange := GridPosChange;
+    FGrid.OnGetCellColor := GridGetCellColor;
+    FGrid.OnGetCellText := GridGetDlgText;
+    FGrid.OnPaintCell := GridPaintCell;
+    FGrid.Options := [goRowSelect {, goFollowMouse} {,goWheelMovePos} ];
+  end;
+
+
+  procedure TFilteredListDlg.InitDialog; {override;}
+  begin
+    SendMsg(DM_SETMOUSEEVENTNOTIFY, 1, 0);
+    ReinitGrid;
+  end;
+
+
+  function TFilteredListDlg.CloseDialog(ItemID :Integer) :Boolean; {override;}
+  begin
+    Result := True;
+  end;
+
+
+  procedure TFilteredListDlg.UpdateHeader; {virtual;}
+  begin
+  end;
+
+
+  procedure TFilteredListDlg.ReinitGrid; {virtual;}
+  begin
+  end;
+
+
+  procedure TFilteredListDlg.ReinitAndSaveCurrent; {virtual;}
+  begin
+  end;
+
+
+  procedure TFilteredListDlg.GridCellClick(ASender :TFarGrid; ACol, ARow :Integer; AButton :Integer; ADouble :Boolean); {virtual;}
+  begin
+//  TraceF('GridCellClick: Pos=%d x %d, Button=%d, Double=%d', [ACol, ARow, AButton, Byte(ADouble)]);
+    if AButton = 1 then
+      SelectItem(IntIf(ADouble, 2, 1));
+  end;
+
+
+  procedure TFilteredListDlg.GridPosChange(ASender :TFarGrid); {virtual;}
+  begin
+    {Abstract}
+  end;
+
+
+  procedure  TFilteredListDlg.GridGetCellColor(ASender :TFarGrid; ACol, ARow :Integer; var AColor :Integer); {virtual;}
+  begin
+    {Abstract}
+  end;
+
+
+  function TFilteredListDlg.GridGetDlgText(ASender :TFarGrid; ACol, ARow :Integer) :TString; {virtual;}
+  begin
+    Result := '';
+  end;
+
+
+  procedure TFilteredListDlg.GridPaintCell(ASender :TFarGrid; X, Y, AWidth :Integer; ACol, ARow :Integer; AColor :Integer); {virtual;}
+  begin
+    {Abstract}
+  end;
+
+
+  procedure TFilteredListDlg.SelectItem(ACode :Integer);  {virtual;}
+  begin
+    {Abstract}
+  end;
+
+
+  function TFilteredListDlg.RowToIdx(ARow :Integer) :Integer;
+  begin
+    if FFilter = nil then
+      Result := ARow
+    else begin
+      Result := -1;
+      if ARow < FFilter.Count then
+        Result := FFilter[ARow];
+    end;
+  end;
+
+
+  function TFilteredListDlg.DialogHandler(Msg :Integer; Param1 :Integer; Param2 :TIntPtr) :TIntPtr; {override;}
+
+    procedure LocSetFilter(const ANewFilter :TString);
+    begin
+      if ANewFilter <> FFilterMask then begin
+//      TraceF('Mask: %s', [ANewFilter]);
+//      FFilterMode := ANewFilter <> '';
+        FFilterMask := ANewFilter;
+        ReinitAndSaveCurrent;
+      end;
+    end;
+
+  begin
+    Result := 1;
+    case Msg of
+      DN_KEY: begin
+        case Param2 of
+          KEY_ENTER:
+            SelectItem(2);
+
+          { Фильтрация }
+          KEY_DEL, KEY_ALT, KEY_RALT:
+            LocSetFilter('');
+          KEY_BS:
+            if FFilterMask <> '' then
+              LocSetFilter( Copy(FFilterMask, 1, Length(FFilterMask) - 1));
+
+          KEY_ADD      : LocSetFilter( FFilterMask + '+' );
+          KEY_SUBTRACT : LocSetFilter( FFilterMask + '-' );
+          KEY_DIVIDE   : LocSetFilter( FFilterMask + '/' );
+          KEY_MULTIPLY : LocSetFilter( FFilterMask + '*' );
+
+        else
+//        TraceF('Key: %d', [Param2]);
+          if (Param2 >= 32) and (Param2 < $FFFF) then
+            LocSetFilter(FFilterMask + WideChar(Param2))
+          else
+            Result := inherited DialogHandler(Msg, Param1, Param2);
+        end;
+      end;
+
+    else
+      Result := inherited DialogHandler(Msg, Param1, Param2);
+    end;
+  end;
 
 
 end.
