a6107fb85042a4b134f98cf37473628af58c1a37
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-09 13:38:04 +0000

    SameFolder v5

--- SameFolder/SameFolder.dpr
+++ SameFolder/SameFolder.dpr
@@ -1,7 +1,9 @@
 {$I Defines.inc}
 
 {$APPTYPE CONSOLE}
-{$ImageBase $40D00000}
+{$ifdef Debug}
+ {$ImageBase $40D00000}
+{$endif Debug}
 
 library SameFolder;
 
@@ -10,17 +12,29 @@ uses
  {$ifdef bTrace}
   MixCheck,
  {$endif bTrace}
+  FarPlug,
   SameFolderMain;
 
 exports
+ {$ifdef Far3}
+  GetGlobalInfoW,
+ {$else}
   GetMinFarVersionW,
+ {$endif Far3}
   SetStartupInfoW,
   GetPluginInfoW,
+ {$ifdef Far3}
+  OpenW,
+  ProcessConsoleInputW,
+ {$else}
   OpenPluginW,
+ {$endif Far3}
   ConfigureW,
   ProcessSynchroEventW,
   ExitFARW;
 
 {$R SameFolder.res}
 
+begin
+  Plug := TSameFolderPlug.Create;
 end.
