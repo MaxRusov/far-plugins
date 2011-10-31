eb029a12baf6b5447ec36bd753701f59beb2b701
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:02:12 +0000

    EdtFind update

--- trunk/EdtFind/fpc.bat
+++ trunk/EdtFind/fpc.bat
@@ -1,11 +1,2 @@
 @Echo off
-
-if not exist ..\Units\EdtFind md ..\Units\EdtFind
-if not exist ..\Bin\EdtFind md ..\Bin\EdtFind
-
-windres -i EdtFindW.rc  -o EdtFindW.RES || exit
-
-fpc.exe -B EdtFind.dpr %* || exit
-
-copy Doc\* ..\Bin\EdtFind
-
+call ..\_fpc.bat EdtFind %*
--- trunk/EdtFind/fpc64.bat
+++ trunk/EdtFind/fpc64.bat
@@ -1,11 +1,2 @@
 @Echo off
-
-if not exist ..\Units64\EdtFind md ..\Units64\EdtFind
-if not exist ..\Bin64\EdtFind md ..\Bin64\EdtFind
-
-windres -i EdtFindW.rc  -o EdtFindW.RES || exit
-
-ppcrossx64.exe -B EdtFind.dpr %* || exit
-
-copy Doc\* ..\Bin64\EdtFind
-
+call ..\_fpc.bat EdtFind 64 %*
