53c65765dd1c71799a63ea0bd155c80e1c7e84cb
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:05:35 +0000

    VisualCompare update

--- trunk/VisualCompare/dcc.bat
+++ trunk/VisualCompare/dcc.bat
@@ -1,14 +1,2 @@
 @Echo off
-
-if not exist ..\Units\VisualCompare md ..\Units\VisualCompare
-if not exist ..\Bin\VisualCompare md ..\Bin\VisualCompare
-
-if exist VisComp.cfg del VisComp.cfg
-
-brcc32 VisCompA.rc || exit
-brcc32 VisCompW.rc || exit
-
-dcc32.exe -B VisComp.dpr %* || exit
-
-copy Doc\* ..\Bin\VisualCompare
-if /i "%1"=="-dunicode" copy DocW\* ..\Bin\VisualCompare
+call ..\_dcc.bat VisComp %*
--- trunk/VisualCompare/fpc64.bat
+++ trunk/VisualCompare/fpc64.bat
@@ -1,11 +1,2 @@
 @Echo off
-
-if not exist ..\Units64\VisualCompare md ..\Units64\VisualCompare
-if not exist ..\Bin64\VisualCompare md ..\Bin64\VisualCompare
-
-windres -i VisCompW.rc  -o VisCompW.RES || exit
-
-ppcrossx64.exe -B VisComp.dpr %* || exit
-
-copy Doc\* ..\Bin64\VisualCompare
-
+call ..\_fpc.bat VisComp 64 %*
