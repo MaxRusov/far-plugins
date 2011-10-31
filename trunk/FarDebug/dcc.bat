8843bec6a4d56bbdb046dfe5b3c9664eafbf2558
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:04:20 +0000

    FarDebug update

--- trunk/FarDebug/dcc.bat
+++ trunk/FarDebug/dcc.bat
@@ -1,15 +1,2 @@
 @Echo off
-
-if not exist ..\Units\FarDebug md ..\Units\FarDebug
-if not exist ..\Bin\FarDebug md ..\Bin\FarDebug
-if exist FarDebug.cfg del FarDebug.cfg
-
-brcc32 FarDebugA.rc || exit
-brcc32 FarDebugW.rc || exit
-
-dcc32.exe -B FarDebug.dpr %* || exit
-
-copy Doc\* ..\Bin\FarDebug
-if /i "%1"=="-dunicode" copy DocW\* ..\Bin\FarDebug
-
-
+call ..\_dcc.bat FarDebug %*
