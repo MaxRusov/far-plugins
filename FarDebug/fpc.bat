262e56f200dfd3e079cd467750ba80d6e3348ac4
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:04:20 +0000

    FarDebug update

--- FarDebug/fpc.bat
+++ FarDebug/fpc.bat
@@ -1,12 +1,2 @@
 @Echo off
-
-if not exist ..\Units\FarDebug md ..\Units\FarDebug
-if not exist ..\Bin\FarDebug md ..\Bin\FarDebug
-
-windres -i FarDebugA.rc  -o FarDebugA.RES || exit
-windres -i FarDebugW.rc  -o FarDebugW.RES || exit
-
-fpc.exe -B FarDebug.dpr %* || exit
-
-copy Doc\* ..\Bin\FarDebug
-if /i "%1"=="-dunicode" copy DocW\* ..\Bin\FarDebug
+call ..\_fpc.bat FarDebug %*
