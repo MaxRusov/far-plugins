9e2ef695ddde3371f740f95bdbb437ab6d3c09a3
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:01:01 +0000

    FarTab update

--- trunk/FarTabs/fpc.bat
+++ trunk/FarTabs/fpc.bat
@@ -1,12 +1,2 @@
 @Echo off
-
-if not exist ..\Units\PanelTabs md ..\Units\PanelTabs
-if not exist ..\Bin\PanelTabs md ..\Bin\PanelTabs
-
-windres -i PanelTabsA.rc -o PanelTabsA.RES || exit
-windres -i PanelTabsW.rc -o PanelTabsW.RES || exit
-
-fpc.exe -B PanelTabs.dpr %* || exit
-
-copy Doc\* ..\Bin\PanelTabs
-if /i "%1"=="-dunicode" (copy DocW\* ..\Bin\PanelTabs) else (copy DocA\* ..\Bin\PanelTabs)
+call ..\_fpc.bat PanelTabs %*
--- trunk/FarTabs/fpc64.bat
+++ trunk/FarTabs/fpc64.bat
@@ -1,12 +1,2 @@
 @Echo off
-
-if not exist ..\Units64\PanelTabs md ..\Units64\PanelTabs
-if not exist ..\Bin64\PanelTabs md ..\Bin64\PanelTabs
-
-windres -i PanelTabsA.rc -o PanelTabsA.RES || exit
-windres -i PanelTabsW.rc -o PanelTabsW.RES || exit
-
-ppcrossx64.exe -B PanelTabs.dpr %* || exit
-
-copy Doc\* ..\Bin64\PanelTabs
-if /i "%1"=="-dunicode" (copy DocW\* ..\Bin64\PanelTabs) else (copy DocA\* ..\Bin64\PanelTabs)
+call ..\_fpc.bat PanelTabs 64 %*
