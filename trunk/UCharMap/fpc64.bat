620a5e9ff2f1895e48665aaed61fa7f24ff12427
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:06:28 +0000

    UCharMap update

--- trunk/UCharMap/fpc64.bat
+++ trunk/UCharMap/fpc64.bat
@@ -1,11 +1,2 @@
 @Echo off
-
-if not exist ..\Units64\UCharMap md ..\Units64\UCharMap
-if not exist ..\Bin64\UCharMap md ..\Bin64\UCharMap
-
-windres -i UCharMapW.rc -o UCharMapW.RES || exit
-
-ppcrossx64.exe -B UCharMap.dpr %* || exit
-
-copy Doc\* ..\Bin64\UCharMap
-rem if /i "%1"=="-dunicode" copy DocW\* ..\Bin64\UCharMap
+call ..\_fpc.bat UCharMap 64 %*
