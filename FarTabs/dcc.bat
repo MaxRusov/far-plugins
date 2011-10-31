303ec9697d3db98488416d24275dc5cc469a0a0f
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:01:01 +0000

    FarTab update

--- FarTabs/dcc.bat
+++ FarTabs/dcc.bat
@@ -1,14 +1,2 @@
 @Echo off
-
-if not exist ..\Units\PanelTabs md ..\Units\PanelTabs
-if not exist ..\Bin\PanelTabs md ..\Bin\PanelTabs
-if exist PanelTabs.cfg del PanelTabs.cfg
-
-brcc32 PanelTabsA.rc || exit
-brcc32 PanelTabsW.rc || exit
-
-dcc32.exe -B PanelTabs.dpr %* || exit
-
-copy Doc\* ..\Bin\PanelTabs
-copy DocW\* ..\Bin\PanelTabs
-rem if /i "%1"=="-dunicode" (copy DocW\* ..\Bin\PanelTabs) else (copy DocA\* ..\Bin\PanelTabs)
+call ..\_dcc.bat PanelTabs %*
