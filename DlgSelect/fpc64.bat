f6079967054b2bccab7e2364aba8aaf200357564
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-10-31 23:06:05 +0000

    DlgSelect update

--- DlgSelect/fpc64.bat
+++ DlgSelect/fpc64.bat
@@ -1,11 +1,2 @@
 @Echo off
-
-if not exist ..\Units64\DlgSelect md ..\Units64\DlgSelect
-if not exist ..\Bin64\DlgSelect md ..\Bin64\DlgSelect
-
-windres -i DlgSelect.rc  -o DlgSelect.RES || exit
-
-ppcrossx64.exe -B DlgSelect.dpr %* || exit
-
-copy Doc\* ..\Bin64\DlgSelect
-
+call ..\_fpc.bat DlgSelect 64 %*
