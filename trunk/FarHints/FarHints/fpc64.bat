d4c04fa4afb23cef4c8c36961ede068a5f16454a
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-11-17 19:11:40 +0000

    FarHints update

--- trunk/FarHints/FarHints/fpc64.bat
+++ trunk/FarHints/FarHints/fpc64.bat
@@ -1,6 +1,6 @@
 @Echo off
 
-Set BinPath=..\..\Bin64\FarHints
+Set BinPath=..\..\Bin_64\FarHints
 Set DcuPath=..\..\Units64\FarHints\FarHints
 
 if not exist "%BinPath%" md %BinPath%
@@ -9,7 +9,7 @@ if not exist "%DcuPath%" md %DcuPath%
 windres -i FarHintsA.rc  -o FarHintsA.RES || exit
 windres -i FarHintsW.rc  -o FarHintsW.RES || exit
 
-ppcrossx64.exe -B FarHints.dpr %* || exit
+ppcrossx64.exe -B -FE%BinPath% FarHints.dpr %* || exit
 
 copy Doc\* %BinPath%
 if /i "%1"=="-dansi" (copy DocA\* %BinPath%) else (copy DocW\* %BinPath%)
