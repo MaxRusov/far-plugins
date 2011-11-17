4473b150918ebac0c5d5eec5b4fc880c962535ed
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-11-17 19:13:06 +0000

    GDIPlus update

--- PicViewGDI/dcc.bat
+++ PicViewGDI/dcc.bat
@@ -1,9 +1,2 @@
 @Echo off
-
-if not exist ..\Units\PictureView md ..\Units\PictureView
-if not exist ..\Bin\PictureView md ..\Bin\PictureView
-if exist GDIPlus.cfg del GDIPlus.cfg
-
-brcc32 GDIPlus.rc || exit
-
-dcc32.exe -B GDIPlus.dpr %* || exit
+call ..\_dcc.bat GDIPlus %*
