141337ede6eccfe0c327a1e3e44c69f10f718509
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2011-11-17 18:58:06 +0000

    SameFolder update

--- SameFolder/fpc64.bat
+++ SameFolder/fpc64.bat
@@ -1,11 +1,2 @@
 @Echo off
-
-if not exist ..\Units64\SameFolder md ..\Units64\SameFolder
-if not exist ..\Bin64\SameFolder md ..\Bin64\SameFolder
-
-windres -i SameFolder.rc  -o SameFolder.RES || exit
-
-ppcrossx64.exe -B SameFolder.dpr %* || exit
-
-copy Doc\* ..\Bin64\SameFolder
-
+call ..\_fpc.bat SameFolder 64 %*
