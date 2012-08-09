c613e5d5a6f6e0925479d856d5afdbdd4ef02b5f
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2012-08-09 21:10:31 +0000

    Test plugin update

--- trunk/TestPlugin1/fpc64.bat
+++
@@ -1,8 +0,0 @@
-@Echo off
-
-if not exist ..\Units64\HelloWorld md ..\Units64\HelloWorld
-if not exist ..\Bin64\HelloWorld md ..\Bin64\HelloWorld
-
-ppcrossx64.exe -B HelloWorld.dpr %* || exit
-
-copy Doc\* ..\Bin64\HelloWorld
