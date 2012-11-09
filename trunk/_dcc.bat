3441dc8991bf278c045b75987cd066f608f6fac2
Max.Rusov <Max.Rusov@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2012-11-09 10:48:50 +0000

    Api Update

--- trunk/_dcc.bat
+++ trunk/_dcc.bat
@@ -4,36 +4,28 @@ shift
 
 if /i "%1" == "64" (
   set Platform=x64
-  set Compiler=
+  set Platform1=x64
+  set Compiler=dcc64.exe
   shift
 ) else (
   set Platform=x32
+  set Platform1=
   set Compiler=dcc32.exe
 )
 
 if /i "%1" == "Far3" (
-  if "%Platform%" == "x32" (
-    set Bin=Bin3
-  ) else (
-    set Bin=Bin3x64
-  )
-  set DefVer=-dFar3
   set FarVer=3
   shift
 ) else (
-  if "%Platform%" == "x32" (
-    set Bin=Bin
-  ) else (
-    set Bin=Bin_64
-  )
-  set DefVer=
   set FarVer=2
 )
 
-if "%Platform%" == "x32" (
-  set Units=Units
-) else (
+set Bin=Bin%FarVer%%Platform1%
+rem set Units=Units%Platform1%
+if "%Platform%" == "x64" (
   set Units=Units64
+) else (
+  set Units=Units
 )
 
 if exist ..\MixLib (
@@ -42,21 +34,26 @@ if exist ..\MixLib (
   set Root=..\..
 )
 
+if "%BinFolder%" == "" (
+  set Dest=%Root%\%Bin%\%PrjName%
+) else (
+  set Dest=%Root%\%Bin%\%BinFolder%
+)
 
 if exist %PrjName%.cfg del %PrjName%.cfg
 
 if not exist %Root%\%Units%\%PrjName% md %Root%\%Units%\%PrjName%
-if not exist %Root%\%Bin%\%PrjName% md %Root%\%Bin%\%PrjName%
+if not exist %Dest% md %Dest%
 
 set ResDef=-dFar%FarVer% -d%Platform%
 if exist %PrjName%.rc brcc32 %ResDef% %PrjName%.rc || exit
 if exist %PrjName%A.rc brcc32 %ResDef% %PrjName%A.rc || exit
 if exist %PrjName%W.rc brcc32 %ResDef% %PrjName%W.rc || exit
 
-%Compiler% -B -E%Root%\%Bin%\%PrjName% %DefVer% %PrjName%.dpr %1 %2 %3 %4 %5 %6 %7 %8 %9 || exit
+%Compiler% -B -E%Dest% -dFar%FarVer% %PrjName%.dpr %1 %2 %3 %4 %5 %6 %7 %8 %9 || exit
 
-if exist Doc copy Doc\* %Root%\%Bin%\%PrjName% >nul
-if exist DocW copy DocW\* %Root%\%Bin%\%PrjName% >nul
+if exist Doc copy Doc\* %Dest% >nul
+if exist DocW copy DocW\* %Dest% >nul
 
-if exist Doc%FarVer% copy Doc%FarVer%\* %Root%\%Bin%\%PrjName% >nul
+if exist Doc%FarVer% copy Doc%FarVer%\* %Dest% >nul
 
--- trunk/_fpc.bat
+++ trunk/_fpc.bat
@@ -4,36 +4,28 @@ shift
 
 if /i "%1" == "64" (
   set Platform=x64
+  set Platform1=x64
   set Compiler=ppcrossx64.exe
   shift
 ) else (
   set Platform=x32
+  set Platform1=
   set Compiler=fpc.exe
 )
 
 if /i "%1" == "Far3" (
-  if "%Platform%" == "x32" (
-    set Bin=Bin3
-  ) else (
-    set Bin=Bin3x64
-  )
-  set DefVer=-dFar3
   set FarVer=3
   shift
 ) else (
-  if "%Platform%" == "x32" (
-    set Bin=Bin
-  ) else (
-    set Bin=Bin_64
-  )
-  set DefVer=
   set FarVer=2
 )
 
-if "%Platform%" == "x32" (
-  set Units=Units
-) else (
+set Bin=Bin%FarVer%%Platform1%
+rem set Units=Units%Platform1%
+if "%Platform%" == "x64" (
   set Units=Units64
+) else (
+  set Units=Units
 )
 
 if exist ..\MixLib (
@@ -42,18 +34,23 @@ if exist ..\MixLib (
   set Root=..\..
 )
 
+if "%BinFolder%" == "" (
+  set Dest=%Root%\%Bin%\%PrjName%
+) else (
+  set Dest=%Root%\%Bin%\%BinFolder%
+)
 
 if not exist %Root%\%Units%\%PrjName% md %Root%\%Units%\%PrjName%
-if not exist %Root%\%Bin%\%PrjName% md %Root%\%Bin%\%PrjName%
+if not exist %Dest% md %Dest%
 
 set ResDef=-DFar%FarVer% -D%Platform%
 if exist %PrjName%.rc windres %ResDef% -i %PrjName%.rc  -o %PrjName%.RES || exit
 if exist %PrjName%A.rc windres %ResDef% -i %PrjName%A.rc  -o %PrjName%A.RES || exit
 if exist %PrjName%W.rc windres %ResDef% -i %PrjName%W.rc  -o %PrjName%W.RES || exit
 
-%Compiler% -B -FE%Root%\%Bin%\%PrjName% %DefVer% %PrjName%.dpr %1 %2 %3 %4 %5 %6 %7 %8 %9 || exit
+%Compiler% -B -FE%Dest% -dFar%FarVer% %PrjName%.dpr %1 %2 %3 %4 %5 %6 %7 %8 %9 || exit
 
-if exist Doc copy Doc\* %Root%\%Bin%\%PrjName% >nul
-if exist DocW copy DocW\* %Root%\%Bin%\%PrjName% >nul
+if exist Doc copy Doc\* %Dest% >nul
+if exist DocW copy DocW\* %Dest% >nul
 
-if exist Doc%FarVer% copy Doc%FarVer%\* %Root%\%Bin%\%PrjName% >nul
+if exist Doc%FarVer% copy Doc%FarVer%\* %Dest% >nul
