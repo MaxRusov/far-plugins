92518ac4b87cb8b58d9026111f0e48ee5e9d4b85
max.rusov@gmail.com <max.rusov@gmail.com@2e2b9662-79f4-11de-9499-4f8beaa274e3>
2014-11-21 19:34:16 +0000

    Noisy Update

--- trunk/Noisy/GUI/_dcc.bat
+++ trunk/Noisy/GUI/_dcc.bat
@@ -1,5 +1,8 @@
 @Echo off
 
-if not exist ..\..\Units\Noisy\GUI md ..\..\Units\Noisy\GUI
+rem if not exist ..\..\Units\Noisy\GUI md ..\..\Units\Noisy\GUI
+
+set BinFolder=Noisy
+set UnitsFolder=Noisy\GUI
 
 call ..\..\_dcc.bat WinNoisy %*
